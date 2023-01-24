4f8722f4b13996aa4c55913034d03d3d6af2dece
arashiyama11 <arashiyama1120@gmail.com>
2023-01-24 20:03:10 +0900

    implement BottomNavigationBar

--- app/src/main/java/io/github/arashiyama11/mojishikiapp/MainActivity.kt
+++ app/src/main/java/io/github/arashiyama11/mojishikiapp/MainActivity.kt
@@ -13,6 +13,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.arashiyama11.mojishikiapp.ui.theme.MojishikiAppTheme
 import io.github.arashiyama11.Polynomial
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,10 +27,10 @@ class MainActivity : ComponentActivity() {
         // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
           Column() {
-            Greeting("Android")
             Text(text = Polynomial("x^2+4x+3").factorization().toString())
             Text(text = Polynomial("x^2+4x+3").solve().toString())
           }
+          BottomNavigationBar()
         }
       }
     }
@@ -35,6 +40,64 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
   Text(text = "Hello $name!")
+}
+
+@Composable
+private fun BottomNavigationBar(modifier: Modifier = Modifier) {
+  BottomNavigation(modifier) {
+    BottomNavigationItem(
+      icon = {
+        Icon(
+          imageVector = Icons.Default.Spa,
+          contentDescription = null
+        )
+      },
+      label = {
+        Text("式の整理")
+      },
+      selected = true,
+      onClick = {}
+    )
+    BottomNavigationItem(
+      icon = {
+        Icon(
+          imageVector = Icons.Default.SafetyDivider,
+          contentDescription = null
+        )
+      },
+      label = {
+        Text("因数分解")
+      },
+      selected = false,
+      onClick = {}
+    )
+    BottomNavigationItem(
+      icon = {
+        Icon(
+          imageVector = Icons.Default.Groups,
+          contentDescription = null
+        )
+      },
+      label = {
+        Text("連立方程式")
+      },
+      selected = false,
+      onClick = {}
+    )
+    BottomNavigationItem(
+      icon = {
+        Icon(
+          imageVector = Icons.Default.Circle,
+          contentDescription = null
+        )
+      },
+      label = {
+        Text("微分積分")
+      },
+      selected = false,
+      onClick = {}
+    )
+  }
 }
 
 @Preview(showBackground = true)
